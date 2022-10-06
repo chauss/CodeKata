a27586ba9b0219d81685d238ba6109703d3d32ca
Christopher Hauß <christopher_hauss@gmx.de>
2022-10-06 16:34:13 +0200

    first setup for sudokuSolver

---
+++ SudokuSolver/SudokuSolverCustom.go
@@ -0,0 +1,7 @@
+package main
+
+
+
+func SolveSudoku(sudoku [][]int) {
+
+}
---
+++ SudokuSolver/SudokuSolver_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestIndexOfRecursive(t *testing.T) {
+	// TODO test something
+}
