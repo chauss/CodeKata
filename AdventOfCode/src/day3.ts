8a4ac418a6b42bbec7444b2f4c64af3b7bac4aee
Christopher Hauß <christopher_hauss@gmx.de>
2022-12-14 08:34:55 +0100

    added day3 part2

--- AdventOfCode/src/day3.ts
+++ AdventOfCode/src/day3.ts
@@ -39,7 +39,7 @@ const findDuplicateItems = (lines: string[]): string[] => {
 };
 
 const inputFile = await readInput("input_day3.txt");
-const lines = inputFile.split("\n");
+const lines = inputFile.split("\n").filter((line) => line.length > 0);
 const duplicateItems = findDuplicateItems(lines);
 
 let prioSum = 0;
@@ -52,10 +52,30 @@ console.log("DAY3");
 console.log("PART1: The sum of the duplicate items priotiry is: " + prioSum);
 
 const elvesTeamBadges: string[] = [];
-let counter = 0;
 
 for (let i = 0; i < lines.length; i += 3) {
-  const elveOne = lines[i];
-  const elveTwo = lines[i + 1];
-  const elveThree = lines[i + 2];
+  const elveOneItems = lines[i];
+  const elveTwoItems = lines[i + 1];
+  const elveThreeItems = lines[i + 2];
+
+  const commonItems = new Set<string>();
+
+  for (const item of elveOneItems) {
+    commonItems.add(item);
+  }
+  for (const item of commonItems) {
+    if (elveTwoItems.indexOf(item) < 0 || elveThreeItems.indexOf(item) < 0) {
+      commonItems.delete(item);
+    }
+  }
+  const [first] = commonItems;
+  elvesTeamBadges.push(first);
 }
+
+let teamPrioSum = 0;
+for (const item of elvesTeamBadges) {
+  console.log(item);
+  teamPrioSum += calculateItemPriority(item);
+}
+
+console.log("PART2: The sum of the team batches priority is: " + teamPrioSum);
